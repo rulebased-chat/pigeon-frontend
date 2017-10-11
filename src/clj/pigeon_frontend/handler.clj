3de6283c18af04843a520c028811c5c6986c2785
Ilmo Raunio <ilmo.raunio@gmail.com>
2017-10-11 23:13:18 +0300

    Add title

--- src/clj/pigeon_frontend/handler.clj
+++ src/clj/pigeon_frontend/handler.clj
@@ -20,7 +20,7 @@
    [:link {:rel  "icon" :type "image/png" :sizes "16x16" :href "/favicon-16x16.png"}]
    [:link {:rel  "manifest" :href "/manifest.json"}]
    [:link {:rel  "mask-icon" :href "/safari-pinned-tab.svg" :color "#5bbad5"}]
-
+   [:title (get env :title)]
    [:meta {:name "theme-color" :content "#ffffff"}]
    [:meta {:charset "utf-8"}]
    [:meta {:name "viewport"
