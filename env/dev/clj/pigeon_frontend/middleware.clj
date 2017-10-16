7c395cfeea6128de0d641c465738eb6229690dc7
Ilmo Raunio <ilmo.raunio@gmail.com>
2017-10-16 23:59:13 +0300

    Add wrap-forwarded-scheme for https redirection (hopefully)

--- env/dev/clj/pigeon_frontend/middleware.clj
+++ env/dev/clj/pigeon_frontend/middleware.clj
@@ -2,11 +2,13 @@
   (:require [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
             [prone.middleware :refer [wrap-exceptions]]
             [ring.middleware.reload :refer [wrap-reload]]
-            [ring.middleware.webjars :refer [wrap-webjars]]))
+            [ring.middleware.webjars :refer [wrap-webjars]]
+            [ring.middleware.ssl :refer [wrap-forwarded-scheme]]))
 
 (defn wrap-middleware [handler]
   (-> handler
       (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
       wrap-exceptions
       wrap-reload
-      wrap-webjars))
+      wrap-webjars
+      wrap-forwarded-scheme))
